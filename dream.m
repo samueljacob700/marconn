c4f6fa9c21b2d53815c054d29d2e81ec8248f092
Marco Cusumano-Towner <imarcoam@gmail.com>
2012-07-24 00:25:19 -0700

    dream

--- dream.m
+++ dream.m
@@ -6,10 +6,10 @@
    %h4 = rand(nh4,1);
    h3 = sample_down(h4,w4,bv4);
    h4 = sample_up(h3,w4,bh4);
-   h3 = sample_down(h4,w4,bv4);
+   %h3 = sample_down(h4,w4,bv4);
    h2 = sample_down(h3,w3,bv3);
    h1 = sample_down(h2,w2,bv2);
    v = sample_down(h1,w1,bv1);
    viewim(v);
-   pause(0.03);
+   pause(0.01);
  end
