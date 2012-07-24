5f7e2e3b952234a162ce386ef76c73cfa993a788
Marco Cusumano-Towner <imarcoam@gmail.com>
2012-07-23 22:21:06 -0700

    using probs

--- mnist_dbn.m
+++ mnist_dbn.m
@@ -1,24 +1,27 @@
 % mnist_dbn
-load('mnist.mat','data');
-data = double(data);
+load('mnist_all.mat');
+data = [test0;test1;test2;test3;test4;test5;test6;test7;test8;test9;...
+        train0;train1;train2;train3;train4;train5;train6;train7;train8;train9];
+data = double(data')/255;
 ndata = size(data,2);
 data = data(:,randperm(ndata));
-data = data(:,1:1000);
 nv = size(data,1);
 
-epsw = 0.1;
-epsbh = 0.1;
-epsbv = 0.1;
-nepochs = 100;
-batchsize = 100;
-momentum = 0.5;
+params.epsw = 0.1;
+params.epsbh = 0.1;
+params.epsbv = 0.1;
+params.nepochs = 10;
+params.batchsize = 100;
+params.initialmomentum = 0.5;
+params.finalmomentum = 0.9;
+params.weightcost = 0.0002;
 
 % pre-train layer 1
 nh1 = 1000;
 w1 = 0.1*randn(nh1,nv);
 bh1 = zeros(nh1,1);
 bv1 = zeros(nv,1);
-[w1,bh1,bv1,h1] = train_rbm(w1,bh1,bv1,data,nepochs,batchsize,epsw,epsbh,epsbv,momentum);
+[w1,bh1,bv1,h1] = train_rbm(w1,bh1,bv1,data,params);
 save('dbn.mat','w1','bh1','bv1');
 
 % pre-train layer 2
@@ -26,7 +29,7 @@ nh2 = 500;
 w2 = 0.1*randn(nh2,nh1);
 bh2 = zeros(nh2,1);
 bv2 = zeros(nh1,1);
-[w2,bh2,bv2,h2] = train_rbm(w2,bh2,bv2,h1,nepochs,batchsize,epsw,epsbh,epsbv,momentum);
+[w2,bh2,bv2,h2] = train_rbm(w2,bh2,bv2,h1,params);
 save('dbn.mat','w1','bh1','bv1','w2','bh2','bv2');
 
 % pre-train layer 3
@@ -34,7 +37,7 @@ nh3 = 250;
 w3 = 0.1*randn(nh3,nh2);
 bh3 = zeros(nh3,1);
 bv3 = zeros(nh2,1);
-[w3,bh3,bv3,h3] = train_rbm(w3,bh3,bv3,h2,nepochs,batchsize,epsw,epsbh,epsbv,momentum);
+[w3,bh3,bv3,h3] = train_rbm(w3,bh3,bv3,h2,params);
 save('dbn.mat','w1','bh1','bv1','w2','bh2','bv2','w3','bh3','bv3');
 
 % pre-train layer 4
@@ -42,7 +45,7 @@ nh4 = 30;
 w4 = 0.1*randn(nh4,nh3);
 bh4 = zeros(nh4,1);
 bv4 = zeros(nh3,1);
-[w4,bh4,bv4,h4] = train_rbm(w4,bh4,bv4,h3,nepochs,batchsize,epsw,epsbh,epsbv,momentum);
+[w4,bh4,bv4,h4] = train_rbm(w4,bh4,bv4,h3,params);
 save('dbn.mat','w1','bh1','bv1','w2','bh2','bv2','w3','bh3','bv3','w4','bh4','bv4');
 
 % to dream, run alternating gibbs sampling in layer 4 using w4, bh4,
--- train_rbm.m
+++ train_rbm.m
@@ -1,9 +1,16 @@
-function [w,bh,bv,hout] = train_rbm(w,bh,bv,data,nepochs,batchsize,epsw,epsbh,epsbv,momentum)
+function [w,bh,bv,hout] = train_rbm(w,bh,bv,data,params)
 % hout contains a sample from the final model for each data example
+nepochs = params.nepochs;
+batchsize = params.batchsize;
+epsw = params.epsw;
+epsbh = params.epsbh;
+epsbv = params.epsbv;
+initialmomentum = params.initialmomentum;
+finalmomentum = params.finalmomentum;
+weightcost = params.weightcost;
 
 [nv,ndata] = size(data);
 nh = size(bh,1);
-CDk = 1;
 
 assert(rem(ndata,batchsize) == 0);
 nbatch = ndata/batchsize;
@@ -13,43 +20,54 @@ d_bh = zeros(nh,1);
 d_bv = zeros(nv,1);
 
 for epoch=1:nepochs
-    epoch
-    for batch=1:nbatch
-        first = (batch-1)*batchsize+1;
-        last = batch*batchsize;
-        batchdata = data(:,first:last); % nv x batchsize
-        
-        % E_data (exact)
-        p_h_data = 1./(1+exp(bsxfun(@plus,-w*batchdata,-bh))); % nh x batchsize
-        Ew_data = (1/batchsize)*p_h_data*batchdata'; % nh x nv
-        Ebh_data = (1/batchsize)*sum(p_h_data,2); % nh x 1
-        Ebv_data = (1/batchsize)*sum(batchdata,2); % nv x 1
-        
-        % E_model (use CD-K approximation)
-        % initialize Gibbs chain from data
-        h = double(rand(nh,batchsize) < p_h_data); % nh x batchsize
-	hout(:,first:last) = h;
-        for k=1:CDk
-            % sample visible given hidden
-            tmp = -w'*h;
-            p_v_h = 1./(1+exp(bsxfun(@plus,tmp,-bv))); % nv x batchsize
-            v = double(rand(nv,batchsize) < p_v_h);
-            % sample hidden given visible TODO use probabilities--mean field(?)
-            tmp = -w*v;
-            p_h_v = 1./(1+exp(bsxfun(@plus,tmp,-bh))); % nh x batchsize
-            h = double(rand(nh,batchsize) < p_h_v);
-        end
-        Ew_model = (1/batchsize)*(h*v');
-        Ebh_model = (1/batchsize)*sum(h,2);
-        Ebv_model = (1/batchsize)*sum(v,2);
+  fprintf(1,'epoch %d\r',epoch); 
+  errsum = 0;
 
-        % update
-	d_w = momentum*d_w + epsw*(Ew_data - Ew_model);
-	d_bh = momentum*d_bh + epsbh*(Ebh_data - Ebh_model);
-	d_bv = momentum*d_bv + epsbv*(Ebv_data - Ebv_model);
-        w = w + d_w;
-        bh = bh + d_bh;
-        bv = bv + d_bv;
-        
+  for batch=1:nbatch
+    first = (batch-1)*batchsize+1;
+    last = batch*batchsize;
+    batchdata = data(:,first:last); % nv x batchsize
+    
+    % E_data (exact)
+    p_h_data = 1./(1+exp(bsxfun(@plus,-w*batchdata,-bh))); % nh x batchsize
+    Ew_data = (1/batchsize)*p_h_data*batchdata'; % nh x nv
+    Ebh_data = (1/batchsize)*sum(p_h_data,2); % nh x 1
+    Ebv_data = (1/batchsize)*sum(batchdata,2); % nv x 1
+    
+    % E_model (use CD-K approximation)
+    % initialize Gibbs chain from data
+    h = double(rand(nh,batchsize) < p_h_data); % nh x batchsize
+    hout = p_h_data; % data is probabilities
+    %hout(:,first:last) = h;
+    %for k=1:CDk
+    % sample visible given hidden
+    p_v_h = 1./(1+exp(bsxfun(@plus,-w'*h,-bv))); % nv x batchsize
+    v = p_v_h; % TODO using probabilities in place of states
+    %v = double(rand(nv,batchsize) < p_v_h);
+    % sample hidden given visible TODO use probabilities--mean field(?)
+    p_h_v = 1./(1+exp(bsxfun(@plus,-w*v,-bh))); % nh x batchsize
+    h = p_h_v; % TODO using probs in place of states
+    %h = double(rand(nh,batchsize) < p_h_v);
+    %end
+    Ew_model = (1/batchsize)*(h*v');
+    Ebh_model = (1/batchsize)*sum(h,2);
+    Ebv_model = (1/batchsize)*sum(v,2);
+
+    err = sum(sum( (batchdata-v).^2 ));
+    errsum = err + errsum;    
+    if epoch > 5
+      momentum = finalmomentum;
+    else
+      momentum = initialmomentum;
     end
+    
+    % update
+    d_w = momentum*d_w + epsw*(Ew_data - Ew_model);
+    d_bh = momentum*d_bh + epsbh*(Ebh_data - Ebh_model);
+    d_bv = momentum*d_bv + epsbv*(Ebv_data - Ebv_model);
+    w = w + d_w;
+    bh = bh + d_bh;
+    bv = bv + d_bv;
+    
+  end
 end
